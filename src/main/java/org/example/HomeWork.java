9f4f16a88583579c412ac41753d1461e731d5a00
mariabaranova <baranova_maria@rambler.ru>
2024-12-01 11:53:10 +0300

    hw12

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -3,8 +3,7 @@ package org.example;
 
 import lombok.SneakyThrows;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 public class HomeWork {
 
@@ -14,8 +13,44 @@ public class HomeWork {
      */
     @SneakyThrows
     public void frogSteps(InputStream in, OutputStream out) {
+        try (
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out))) {
+            int size = Integer.parseInt(reader.readLine());
+            for (int k = 0; k < size; k++) {
+                int count = 0;
+                int ans = 0;
+                boolean L = true;
+                boolean R = true;
+                char[] destination = reader.readLine().toCharArray();
+                char[] arr = new char[destination.length + 2];
+                for (int i = 0; i < destination.length; i++) {
+                    if (destination[i] == 'R') {
+                        L = false;
+                        arr[i + 1] = 1;
+                    } else R = false;
+                }
+                if (L) {
+                    writer.write(destination.length + 1 + "\n");
+                    continue;
+                }
+                if (R) {
+                    writer.write(1 + "\n");
+                    continue;
+                }
+                for (int i = 1; i <= destination.length; i++) {
+                    if (arr[i] == 0) count++;
+                    else {
+                        ans = Math.max(count, ans);
+                        count = 0;
+                    }
+                    if (i == destination.length) {
+                        ans = Math.max(count, ans);
+                    }
+                }
+                writer.write(ans + 1 + "\n");
+            }
+            writer.flush();
+        }
     }
-
-
-
 }
